a5d1021a460d85dde62d7ab26a24d963e491ad04
Weilong <fan01856472@gmail.com>
2023-02-13 14:18:24 +0800

    feat/web_api: extract gorm from issue model

--- internal/issue/repository/issue_postgres.go
+++ internal/issue/repository/issue_postgres.go
@@ -2,46 +2,91 @@ package repository
 
 import (
 	"go-issues-api/internal/model"
+	_author "go-issues-api/internal/user/repository"
 
 	"gorm.io/gorm"
 )
+
+type Issue struct {
+	ID          uint `gorm:"primaryKey"`
+	Title       string
+	Description string
+	AuthorID    uint
+	Author      _author.User `gorm:"foreignKey:AuthorID"`
+}
+
+func (i *Issue) toModel() *model.Issue {
+	return &model.Issue{
+		ID:          i.ID,
+		Title:       i.Title,
+		Description: i.Description,
+		Author:      model.User(i.Author),
+	}
+}
+
+func toGorm(i *model.Issue) *Issue {
+	return &Issue{
+		Title:       i.Title,
+		Description: i.Description,
+		Author:      _author.User(i.Author),
+	}
+}
 
 type IssueRepository struct {
 	DB *gorm.DB
 }
 
-func NewIssueRepository(db *gorm.DB) *IssueRepository {
+func NewIssueRepository(conn *gorm.DB) *IssueRepository {
 	return &IssueRepository{
-		DB: db,
+		DB: conn,
 	}
 }
 
 func (this *IssueRepository) FindAll() ([]*model.Issue, error) {
-	var issues []*model.Issue
-	res := this.DB.Joins("Author").Find(&issues)
+	var dbissues []*Issue
+	res := this.DB.Joins("Author").Find(&dbissues)
+
+	issues := make([]*model.Issue, 0, len(dbissues))
+	for _, dbi := range dbissues {
+		issues = append(issues, &model.Issue{
+			ID:          dbi.ID,
+			Title:       dbi.Title,
+			Description: dbi.Description,
+			Author: model.User{
+				ID:   dbi.Author.ID,
+				Name: dbi.Author.Name,
+			},
+		})
+	}
 
 	return issues, res.Error
 }
 
 func (this *IssueRepository) Create(issue *model.Issue) error {
-	res := this.DB.Create(issue)
+	dbissue := toGorm(issue)
+	res := this.DB.Create(dbissue)
+
 	return res.Error
 }
 
 func (this *IssueRepository) FindBy(id int) (*model.Issue, error) {
-	var issue *model.Issue
-	res := this.DB.Joins("Author").First(&issue, id)
+	var dbissue *Issue
+	res := this.DB.Joins("Author").First(&dbissue, id)
 
-	return issue, res.Error
+	return dbissue.toModel(), res.Error
 }
 
+// TODO: doesn't really update
 func (this *IssueRepository) Update(issue *model.Issue) (*model.Issue, error) {
-	res := this.DB.Save(&issue)
+	dbissue := toGorm(issue)
+	res := this.DB.Save(&dbissue)
 
-	return issue, res.Error
+	return dbissue.toModel(), res.Error
 }
 
 func (this *IssueRepository) Delete(id int) (int64, error) {
-	res := this.DB.Delete(&model.Issue{}, id)
+	var dbissue Issue
+	res := this.DB.Delete(&dbissue, id)
+
 	return res.RowsAffected, res.Error
 }
--- internal/model/issue.go
+++ internal/model/issue.go
@@ -1,10 +1,10 @@
 package model
 
 type Issue struct {
-	ID          uint   `gorm:"primaryKey" json:"id"`
+	ID          uint   `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
-	Author      User   `gorm:"foreignKey:ID" json:"author"`
+	Author      User   `json:"author"`
 }
 
 type IssueRepository interface {
